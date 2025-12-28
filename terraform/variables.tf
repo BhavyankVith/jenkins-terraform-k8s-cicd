6daec94f22a3d6d9bbb361484f9b1ed428818fd5
BhavyankVith <34795451+BhavyankVith@users.noreply.github.com>
2025-12-28 18:46:58 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,28 @@
+# variable "subnet_ids" {
+#   type = list(string)
+# }
+
+# variable "region" {
+#   type    = string
+#   default = "eu-north-1"
+
+# }
+
+# New appraoch 
+
 variable "subnet_ids" {
-  type = list(string)
+  type        = list(string)
+  description = "List of subnet IDs for the EKS cluster"
+  default     = [] # Adding a default value prevents the 'Required' error
 }
 
 variable "region" {
   type    = string
   default = "eu-north-1"
+}
 
+# Add this block so Terraform accepts the -var="cluster_name=..." from Jenkins
+variable "cluster_name" {
+  type    = string
+  default = "demo-eks"
 }
